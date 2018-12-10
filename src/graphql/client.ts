f875da27f37a6b8ba03f975b92bf0426b37757c1
aromanarguello <aromanarguello@gmail.com>
2018-12-09 21:47:58 -0500

    removed unused dependency, erased comments in client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,14 +1,7 @@
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import ApolloClient from 'apollo-client'
-import { ApolloLink } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
 import { createHttpLink } from 'apollo-link-http'
-
-interface Client {
-    // uri: string
-    link: ApolloLink
-    cache: NormalizedCacheObject
-}
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
